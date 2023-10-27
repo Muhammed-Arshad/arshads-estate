181ef279b942dd25d07f404e68ce37be5f223b9a
Arshad <mdarshadkp786@gmail.com>
2023-10-27 12:28:52 +0400

    hi

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/Signin'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+function App() {
+  return <BrowserRouter>
+  <Header/>
+    <Routes>
+      <Route path='/' element= {<Home/>}></Route>
+      <Route path='/sign-in' element= {<SignIn/>}></Route>
+      <Route path='/sign_up' element= {<SignUp/>}></Route>
+      <Route path='/about' element= {<About/>}></Route>
+      <Route path='/profile' element= {<Profile/>}></Route>
+    </Routes>
+    </BrowserRouter>
+}
+
+export default App
