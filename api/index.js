4f6a2130d2fb0d8b2b815adb517e26aaf1ecb94b
Arshad <mdarshadkp786@gmail.com>
2023-10-28 11:34:07 +0400

    added mongoose

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=> {
+    console.log("Connected to mongo db")
+})
+
+const app = express()
+
+app.listen(3000, ()=> {
+    console.log("Server is running 3000")
+})
+
