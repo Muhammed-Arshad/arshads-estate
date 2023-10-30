7a27c3b416b57c4b8e1361dcf4d92cf1af7c0c37
Arshad <mdarshadkp786@gmail.com>
2023-10-30 10:27:55 +0400

    created sigup api and hash password done

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -9,11 +10,14 @@ mongoose.connect(process.env.MONGO).then(()=> {
     console.log("Connected to mongo db")
 })
 
-const app = express()
+const app = express();
+
+app.use(express.json());
 
 app.listen(3000, ()=> {
     console.log("Server is running 3000")
 })
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
 
