67fb24c779b313619249abd803d393285232b1b4
Arshad <mdarshadkp786@gmail.com>
2023-11-03 14:47:39 +0400

    delete user api and ui done

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,14 +4,22 @@ import {useRef,useEffect} from 'react'
 import {getStorage, ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage'
 import { app } from '../firebase'
 
+import {updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
+import {deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux'
+
 function Profile() {
     const fileRef = useRef(null)
-    const {currentUser} = useSelector(state=> state.user)
+    const {currentUser, loading, error} = useSelector(state=> state.user)
     const [file, setFile] = useState(undefined);
     const [filePerc, setFilePerc] = useState(0);
     const [fileUplaodError, setFileUploadError] = useState(false);
 
     const [formData, setFormData] = useState({})
+
+    const [updateSuccess , setUpdateSuccess] = useState(false)
+    const dispatch = useDispatch();
+
 
     console.log(file)
     console.log(formData);
@@ -48,12 +56,67 @@ function Profile() {
             )
          });
 
+    };
+
+    const handleChange = (e)=>{
+        setFormData({...formData, [e.target.id]: e.target.value});
     }
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        try{
+            dispatch(updateUserStart());
+            const res = await fetch('api/user/update/'+ currentUser._id ,{
+                method : 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            });  
+            
+            const data = await res.json();
+
+            if(data.success === false){
+                dispatch(updateUserFailure(data.message));
+                return;
+            }
+
+            dispatch(updateUserSuccess(data));
+            setUpdateSuccess(true);
+
+        }catch(e){
+            dispatch(updateUserFailure(e.message))
+        }
+    }
+
+
+    const handleDeleteUser = async ()=>{
+        try{
+            dispatch(deleteUserStart());
+
+            const res = await fetch('api/user/delete/'+currentUser._id,{
+                method: 'DELETE',
+            });
+
+            const data = await res.json();
+
+            if(data.success === false){
+                dispatch(deleteUserFailure(data.message));
+                return;
+            }
+
+            dispatch(deleteUserSuccess(data));
+
+        }catch(e){
+            dispatch(deleteUserFailure(e.message))
+        }
+    }
+
 
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-            <form className='flex flex-col gap-4'>
+            <form onSubmit={ handleSubmit} className='flex flex-col gap-4'>
                 <input onChange={(e)=> setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='
                 image/*'/>
                 <img onClick={()=> fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt='profile' 
@@ -71,21 +134,42 @@ function Profile() {
                             </span>
                         ):""}
                     </p>
-                <input type='text' placeholder='username'
-                 id= 'username' className='border p-3 rounded-lg'/>   
-                <input type='email' placeholder='email'
-                id= 'email' className='border p-3 rounded-lg'/> 
-                <input type='text' placeholder='password'
-                id= 'password' className='border p-3 rounded-lg'/>  
-                <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover: opacity-95'>
-                    update
+                <input 
+                    type='text' 
+                    placeholder='username'
+                    id= 'username' 
+                    defaultValue={currentUser.username}
+                    onChange={ handleChange}
+                    className='border p-3 rounded-lg'/>  
+
+                <input 
+                    type='email'
+                    placeholder='email'
+                    id= 'email' 
+                    defaultValue={currentUser.email}
+                    onChange={ handleChange}
+                    className='border p-3 rounded-lg'/>
+
+                <input 
+                    type='password'
+                    placeholder='password'
+                    id= 'password'
+                    onChange={ handleChange}
+                    className='border p-3 rounded-lg'/>  
+
+                <button disabled = {loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover: opacity-95'>
+                    {loading? 'Loading...':'update'}
                 </button>
 
             </form>
             <div className='flex justify-between mt-5'>
-                <span className='text-red-700 cursor-pointer'>Delete Account</span>
+                <span onClick = {handleDeleteUser}className='text-red-700 cursor-pointer'>Delete Account</span>
                 <span className='text-red-700 cursor-pointer'>Sign out</span>
             </div>
+
+            <p className='text-red-700' mt>{error? error:''}</p>
+            <p className='text-green-700' mt>{updateSuccess? 'User updated successfully':''}</p>
+
         </div> 
     )
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -22,10 +22,44 @@ export const userSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
     },
+
+    updateUserStart: (state)=> {
+      state.loading = true
+    },
+
+    updateUserSuccess: (state,action)=>{
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+
+    updateUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    deleteUserStart: (state)=> {
+      state.loading = true
+    },
+
+    deleteUserSuccess: (state,action)=>{
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions
+export const { signInStart, signInSuccess, signInFailure, 
+  updateUserStart,updateUserSuccess,updateUserFailure,
+  deleteUserStart,deleteUserSuccess,deleteUserFailure } = userSlice.actions
 
 export default userSlice.reducer
